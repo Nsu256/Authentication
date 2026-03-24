38ae6f4b99343f2e090dca30cf69a25c26a09a55
NSUBUGA EDIGER <nsubugaediger@gmail.com>
2026-03-24 15:32:13 +0000

    giving the user feedback after login

--- agribuddy_backend/main.py
+++ agribuddy_backend/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, josnfiy
 from flask_cors import CORS
-from werkzeug.security import check-password_harsh
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 CORS(app)
@@ -15,4 +15,6 @@ def login():
 
     user = User.query.filter_by(email=email).first()
     if user and check_password_hash(user.password_hash,password) and user.role == role:
-        
+        return jsonify({'message': 'Login successful', 'role': user.role}),200
+    else:
+        return jsonify({'error': 'Invalid credentials'}),401
